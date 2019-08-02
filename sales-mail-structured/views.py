4ac6f782c59e0aed9fe5efc6110381ad5371f0a8
soichi-fujiwara <so1wisteria@gmail.com>
2019-08-03 08:23:44 +0900

    app_data add

--- sales-mail-structured/views.py
+++ sales-mail-structured/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
  
 def index_template(request):
-  return render(request, 'index.html')
+  myapp_data = {
+    'app': 'Django'
+  }
+  return render(request, 'index.html',myapp_data)
