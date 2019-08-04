c25c0551d0e2951b6e97e79d54882429234d9381
soichi-fujiwara <so1wisteria@gmail.com>
2019-08-04 18:56:42 +0900

    indent edit

--- sales-mail-structured/views.py
+++ sales-mail-structured/views.py
@@ -6,11 +6,11 @@ def index_template(request):
 
   if request.method == "POST":
     form = MyForm(data=request.POST)  # ← 受け取ったPOSTデータを渡す
-      if form.is_valid():  # ← 受け取ったデータの正当性確認
-        pass  # ← 正しいデータを受け取った場合の処理
-      else:  # ← methodが'POST'ではない = 最初のページ表示時の処理
-        #フォーム表示
-        form = MyForm()
+    if form.is_valid():  # ← 受け取ったデータの正当性確認
+      pass  # ← 正しいデータを受け取った場合の処理
+    else:  # ← methodが'POST'ではない = 最初のページ表示時の処理
+      #フォーム表示
+      form = MyForm()
   
   myapp_data = {
     'app': 'Django',
