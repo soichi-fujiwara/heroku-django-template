1dfb5324e9aeb4571ae95947fa4ff8857611c8a5
soichi-fujiwara <so1wisteria@gmail.com>
2019-08-04 18:54:54 +0900

    submit add

--- sales-mail-structured/views.py
+++ sales-mail-structured/views.py
@@ -4,8 +4,13 @@ from .forms import MyForm
 
 def index_template(request):
 
-  #フォーム作成
-  form = MyForm()
+  if request.method == "POST":
+    form = MyForm(data=request.POST)  # ← 受け取ったPOSTデータを渡す
+      if form.is_valid():  # ← 受け取ったデータの正当性確認
+        pass  # ← 正しいデータを受け取った場合の処理
+      else:  # ← methodが'POST'ではない = 最初のページ表示時の処理
+        #フォーム表示
+        form = MyForm()
   
   myapp_data = {
     'app': 'Django',
