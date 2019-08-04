47d9f6dfcee42989ce73faad6c695c34827cfabb
soichi-fujiwara <so1wisteria@gmail.com>
2019-08-04 18:00:45 +0900

    forms add

--- sales-mail-structured/views.py
+++ sales-mail-structured/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
  
 def index_template(request):
+
+  #ƒtƒH[ƒ€ì¬
+  form = MyForm()
+  
   myapp_data = {
-    'app': 'Django'
+    'app': 'Django',
+    'form': form,
   }
   return render(request, 'index.html',myapp_data)
